fee99100c2372f1fa2a406b56807d2a4f80982b9
gyoder <70408179+gyoder@users.noreply.github.com>
2025-06-14 12:46:06 +0100

    added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,22 @@
+set -x PATH /Users/scie/.local/nvim-macos-arm64/bin \
+  /opt/homebrew/Cellar/universal-ctags/HEAD-43ca055/bin \
+  /Users/scie/.local/xonsh-env/xbin \
+  /Users/scie/.local/xonsh-env/xbin \
+  /opt/homebrew/opt/icu4c@76/sbin \
+  /opt/homebrew/opt/icu4c@76/bin \
+  /opt/homebrew/opt/llvm/bin \
+  /Users/scie/.dotnet/tools \
+  /Users/scie/.local/bin \
+  /Users/scie/Library/Python/3.9/bin \
+  /opt/local/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/emulators \
+  /usr/bin \
+  /opt/homebrew/bin/iodine \
+  /opt/homebrew/bin \
+  /Users/scie/.platformio/penv/bin/ \
+  /Users/scie/.cargo/bin \
+  /Users/scie/.local/xonsh-env/xbin \
+  /usr/bin \
+  /bin
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
