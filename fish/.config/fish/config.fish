e89cfe614df8a0f96c3cc6022c257d120ef1da7a
Grace Yoder <70408179+gyoder@users.noreply.github.com>
2025-06-17 08:21:17 -0600

    Added linux path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,23 +1,42 @@
-set -x PATH /Users/scie/.local/nvim-macos-arm64/bin \
-  /opt/homebrew/Cellar/universal-ctags/HEAD-43ca055/bin \
-  /Users/scie/.local/xonsh-env/xbin \
-  /Users/scie/.local/xonsh-env/xbin \
-  /opt/homebrew/opt/icu4c@76/sbin \
-  /opt/homebrew/opt/icu4c@76/bin \
-  /opt/homebrew/opt/llvm/bin \
-  /Users/scie/.dotnet/tools \
-  /Users/scie/.local/bin \
-  /Users/scie/Library/Python/3.9/bin \
-  /opt/local/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/emulators \
-  /opt/homebrew/bin/iodine \
-  /opt/homebrew/bin \
-  /Users/scie/.platformio/penv/bin/ \
-  /Users/scie/.cargo/bin \
-  /Users/scie/.local/xonsh-env/xbin \
-  /usr/bin \
-  /usr/sbin \
-  /bin \
-  /sbin
+set host (hostname)
+
+if test "$host" = "turing"
+    set -x PATH /Users/scie/.local/nvim-macos-arm64/bin \
+      /opt/homebrew/Cellar/universal-ctags/HEAD-43ca055/bin \
+      /Users/scie/.local/xonsh-env/xbin \
+      /Users/scie/.local/xonsh-env/xbin \
+      /opt/homebrew/opt/icu4c@76/sbin \
+      /opt/homebrew/opt/icu4c@76/bin \
+      /opt/homebrew/opt/llvm/bin \
+      /Users/scie/.dotnet/tools \
+      /Users/scie/.local/bin \
+      /Users/scie/Library/Python/3.9/bin \
+      /opt/local/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/emulators \
+      /opt/homebrew/bin/iodine \
+      /opt/homebrew/bin \
+      /Users/scie/.platformio/penv/bin/ \
+      /Users/scie/.cargo/bin \
+      /Users/scie/.local/xonsh-env/xbin \
+      /usr/bin \
+      /usr/sbin \
+      /bin \
+      /sbin
+else if test "$host" = "lovelace"
+    set -x PATH /home/linuxbrew/.linuxbrew/bin \
+      /home/linuxbrew/.linuxbrew/sbin \
+      /home/scie/.nvm/versions/node/v22.15.0/bin \
+      /home/scie/.cargo/bin \
+      /usr/local/sbin \
+      /usr/local/bin \
+      /usr/sbin \
+      /usr/bin \
+      /sbin \
+      /bin \
+      /usr/games \
+      /usr/local/games \
+      /snap/bin
+end
+
 if status is-interactive
   function source_env
     if not test -f .env
