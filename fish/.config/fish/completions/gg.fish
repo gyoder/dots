2aa28f37765e6e8d68a1cbb164445f1af97ca83b
gyoder <70408179+gyoder@users.noreply.github.com>
2025-06-23 13:21:14 -0600

    fish: added loadenv plugin

---
+++ fish/.config/fish/completions/gg.fish
@@ -0,0 +1,1 @@
+complete --command gg --no-files --arguments "(env _GG_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) gg)" --condition "env _GG_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) gg"
