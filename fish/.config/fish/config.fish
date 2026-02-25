dc6f417fa6653330371142dfa18d31cb24d3f334
Grace Yoder <grace_yoder@outlook.com>
2026-02-25 17:25:54 +0000

    added basic brewfile stuff for starting declaritive package management

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -43,20 +43,25 @@ else if test "$host" = "lovelace"
 else if test "$host" = "lee"
   fish_add_path ~/.local/bin
 end
+
+source "$HOME/.cargo/env.fish" 2>/dev/null
+
+set -gx HOMEBREW_BUNDLE_FILE "~/.config/Brewfile"
+
 if test -d /home/linuxbrew/.linuxbrew
   # Homebrew is installed on Linux
 
   set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
   set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar"
   set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew"
-  set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH
+  set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "/home/linuxbrew/.linuxbrew/opt/rustup/bin" $PATH
   set -q MANPATH; or set MANPATH ''
   set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH
   set -q INFOPATH; or set INFOPATH ''
   set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH
 
-  # Homebrew asked for this in order to `brew upgrade`
-  set -gx HOMEBREW_GITHUB_API_TOKEN {api token goes here, don't remember where that's created}
+
+
 end
 
 if status is-interactive
